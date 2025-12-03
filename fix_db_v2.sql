6de6ecb4dda9d399b6a0dc34c5e88da5586ad7f1
artem <pelvity@gmail.com>
2025-12-03 23:16:28 +0100

    chore: remove redundant test files and improve .gitignore

--- fix_db_v2.sql
+++
@@ -1,7 +0,0 @@
-DELETE FROM users;
-
-INSERT INTO features (id, feature, version, type, configs, created_at, updated_at) VALUES 
-(2, 'administrator', 1, 1, '{}', NOW(), NOW()),
-(3, 'early_access', 1, 1, '{}', NOW(), NOW()),
-(4, 'pro_plan_v1', 1, 1, '{"quota": {"blobLimit": 107374182400, "storageLimit": 1099511627776, "historyPeriod": 365, "memberLimit": 100}}', NOW(), NOW())
-ON CONFLICT (id) DO NOTHING;
--- launch-desktop-prod.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Launching AFFiNE Production Desktop Client...
-start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --app=http://localhost:3010
-exit
